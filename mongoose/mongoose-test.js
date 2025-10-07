1b2b07020f4f8376c5c0e99c4c101b1717ffc18c
pritisharma1511 <ps0130815@gmail.com>
2025-10-07 08:38:44 +0530

    done

--- mongoose/mongoose-test.js
+++ mongoose/mongoose-test.js
@@ -1,5 +1,5 @@
 import mongoose from  "mongoose";
-
+//step 1: connect the database
 try{
     await mongoose.connect("mongo://localhost:27017/mongoose_database");
     mongoose.set("debug",true);
@@ -7,3 +7,12 @@ try{
     console.error(error);
     process.exit();
 }
+
+//step2: create schema
+const userSchema = mongoose.Schema({
+    name:{type: String, required: true},
+    email:{type:String,required:true,unique:true},
+    age: { type:Number , required: true,min:5},
+    createdAt:{type:Date,default:Date.now()},
+})
+
