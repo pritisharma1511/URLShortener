0691b795eafa8e662948c82c51aac1a8d159224e
pritisharma1511 <ps0130815@gmail.com>
2025-10-07 08:53:15 +0530

    done

--- mongoose/mongoose-test.js
+++ mongoose/mongoose-test.js
@@ -1,9 +1,9 @@
 import mongoose from  "mongoose";
 //step 1: connect the database
 try{
-    await mongoose.connect("mongo://localhost:27017/mongoose_database");
+    await mongoose.connect("mongodb://127.0.0.1/mongoose_database");
     mongoose.set("debug",true);
-}catch{
+}catch(error){
     console.error(error);
     process.exit();
 }
