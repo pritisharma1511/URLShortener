eedab89a9b03453ecd7d58e1f63fb2e7d98f22fc
pritisharma1511 <ps0130815@gmail.com>
2025-10-07 08:43:22 +0530

    mongosse cretaed

--- mongoose/mongoose-test.js
+++ mongoose/mongoose-test.js
@@ -14,5 +14,12 @@ const userSchema = mongoose.Schema({
     email:{type:String,required:true,unique:true},
     age: { type:Number , required: true,min:5},
     createdAt:{type:Date,default:Date.now()},
-})
+});
+
+//step2 : creating model
+const Users = mongoose.model("user",userSchema);
+
+await Users.create({name:"Priti",age:20,email:"pritisharma@gmail.com"});
+
+await mongoose.connection.close();
 
